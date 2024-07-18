c38972120b9a42776e0be51f7e22b22225314428
sreerag-13 <sreeragsaji41@gmail.com>
2024-07-18 22:07:36 +0530

    token complete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const mongoose=require("mongoose")
 const cors=require("cors")
+const jwt=require("jsonwebtoken")
 const {blogmodel}=require("./models/blog")
 const bcrypt =require("bcryptjs")
 let app=express()
@@ -19,41 +20,53 @@ let input=req.body
 blogmodel.find({"Email":req.body.Email}).then((response)=>{
   if(response.length>0)
     {
-       let dpassword=response[0].Password
-       console.log(dpassword)
-       bcrypt.compare(input.Password,dpassword,(error,IsMatch)=>{
-        if(IsMatch)
+       const dpassword=bcrypt.compareSync(input.Password,response[0].Password)
+        if(dpassword)
             {
-                res.json({"status":"success","user_id":response[0]._id})
+                jwt.sign({Email:input.Email},"blogApp",{expiresIn:"1d"},(error,token)=>{
+                    if(error)
+                    {
+                        res.json({"status":"eroor","errorMessage":error}) 
+                    }
+                    else{
+                        res.json({"status":"success","token":token,"userid":response[0]._id})
+                    }
+                })
+               
             }
             else{
                 res.json({"status":"incorrect password"})
             }
-       })
-    }
+        }
     else{
         res.json({"status":"incorrect email"})
     }
-
-}
-
-
-
-).catch()
-
-
 }
 )
+.catch()
+}
+)
+
+
+
+
+
 
 app.post("/sign",async(req,res)=>{
     let input=req.body
     let hashedpassword= await generateHashedPassword(input.Password)
-    console.log(hashedpassword)
     input.Password=hashedpassword
-    let user=new blogmodel(input)
-    console.log(user)
-    user.save()
-    res.json({"status":"success"})
+    blogmodel.find({Email:input.Email}).then((items)=>{
+     if(items.length>0){
+        res.json({"status":"email is already exit"})
+     }
+     else{
+        let user=new blogmodel(input)
+        user.save()
+        res.json({"status":"success"})
+     }
+    }).catch()
+    
 })
 
 app.listen("8080",()=>{
